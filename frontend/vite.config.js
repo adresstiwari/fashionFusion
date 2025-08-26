a3bbfc756cc713acb94a8a7c7e1bc86f66b7e248
Adarsh <adress2762000@gmail.com>
2025-08-26 14:03:12 +0530

    Fix VITE-API

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
-// https://vitejs.dev/config/
+// Load .env variables
+dotenv.config()
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
+        target: process.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
+        changeOrigin: true,
       }
     }
   }
